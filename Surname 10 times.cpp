b68ba0cb4db52940d75b6c9ac52a38f9a8a2363d
vital8302 <44236481+vital8302@users.noreply.github.com>
2019-01-24 20:12:56 +0200

    Add files via upload

---
+++ Surname 10 times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, ".1251");
+
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << "Пупченко Вiталiй\n";
+	}
+	system("pause");
+	return 0;
+}
